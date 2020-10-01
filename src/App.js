80543ff567c823eaecd17403b7c8d79e4b36ce91
meo098 <meo098@o2.pl>
2020-10-01 19:29:41 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -16,6 +16,7 @@ const App = () => {
     return (
         <Router>
             <Navbar />
+            
             {/* <div>Icons made by <a href="https://www.flaticon.com/authors/freepik" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a></div> */}
             <Switch>
                 <Route path='/' exact>
