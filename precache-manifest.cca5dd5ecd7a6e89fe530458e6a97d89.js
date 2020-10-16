012b6539baf0a25c6ff8292d94db7b64178c53d1
meo098 <meo098@o2.pl>
2020-10-16 20:03:30 +0200

    Updates

---
+++ precache-manifest.cca5dd5ecd7a6e89fe530458e6a97d89.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b2fd11173ef227f281b45acbc688a463",
+    "url": "/kajaki3/index.html"
+  },
+  {
+    "revision": "f9180d09e8a3a9d492f4",
+    "url": "/kajaki3/static/css/main.cdc9d70f.chunk.css"
+  },
+  {
+    "revision": "9572cc7e9e25ab2ab88c",
+    "url": "/kajaki3/static/js/2.ce48fcd5.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/kajaki3/static/js/2.ce48fcd5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f9180d09e8a3a9d492f4",
+    "url": "/kajaki3/static/js/main.000b8efe.chunk.js"
+  },
+  {
+    "revision": "46a9dc0174ac1b017614",
+    "url": "/kajaki3/static/js/runtime-main.c0ea8ccc.js"
+  },
+  {
+    "revision": "147ea5f9575bd47682ab719caec596e0",
+    "url": "/kajaki3/static/media/about2.147ea5f9.jpg"
+  },
+  {
+    "revision": "94b20dff65d979b78b9bb6793af3e576",
+    "url": "/kajaki3/static/media/contact.94b20dff.jpg"
+  },
+  {
+    "revision": "955460099d3b0880ecb2734eb2802e2a",
+    "url": "/kajaki3/static/media/facebook.95546009.svg"
+  },
+  {
+    "revision": "b1c58ee19d1e2c38b29ae86f3174f2e4",
+    "url": "/kajaki3/static/media/instagram.b1c58ee1.svg"
+  },
+  {
+    "revision": "cc80b8b9d156a5c2a66e07bc0931e2bd",
+    "url": "/kajaki3/static/media/kajak1.cc80b8b9.jpg"
+  },
+  {
+    "revision": "33af185a3cc6e745e63eac46eb860322",
+    "url": "/kajaki3/static/media/kajak3.33af185a.jpg"
+  },
+  {
+    "revision": "a68b4770c7d7b939bf24f3232782adf9",
+    "url": "/kajaki3/static/media/kajak4.a68b4770.jpg"
+  },
+  {
+    "revision": "e5fabc5be280a7a7f471d64c577d3d8b",
+    "url": "/kajaki3/static/media/kajak5.e5fabc5b.jpg"
+  },
+  {
+    "revision": "a92108696ceca92a8c8151530c973026",
+    "url": "/kajaki3/static/media/kajak7.a9210869.jpg"
+  },
+  {
+    "revision": "92837f2d1edafedf2842c36eb6d10fa3",
+    "url": "/kajaki3/static/media/logo.92837f2d.svg"
+  },
+  {
+    "revision": "30e01be97a08343a42204354a2538eb4",
+    "url": "/kajaki3/static/media/offer1.30e01be9.jpg"
+  },
+  {
+    "revision": "2362455aa58fdfb9b1cafa1481d7bd68",
+    "url": "/kajaki3/static/media/offer2.2362455a.jpg"
+  },
+  {
+    "revision": "2c8d2f77058bc2007f55d5c84190be32",
+    "url": "/kajaki3/static/media/offer3.2c8d2f77.jpg"
+  },
+  {
+    "revision": "ffa912aac4e65f9a38c9e4a31a7c38f0",
+    "url": "/kajaki3/static/media/person1.ffa912aa.jpg"
+  },
+  {
+    "revision": "486f2396c4a7928ddee07ca627066fe3",
+    "url": "/kajaki3/static/media/person2.486f2396.jpg"
+  },
+  {
+    "revision": "c9f1200836515fd4bacd29160be78732",
+    "url": "/kajaki3/static/media/twitter.c9f12008.svg"
+  },
+  {
+    "revision": "20597e71e95d6a9e0db3dce2fc376cdc",
+    "url": "/kajaki3/static/media/youtube.20597e71.svg"
+  }
+]);
